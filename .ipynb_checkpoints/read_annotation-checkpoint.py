8f33e80bf252fa8a4501035b94dd29041ad95458
Richmond <richmond.alake@gmail.com>
2019-07-29 20:36:16 +0100

    sorted input targets error

--- .ipynb_checkpoints/read_annotation-checkpoint.py
+++ .ipynb_checkpoints/read_annotation-checkpoint.py
@@ -1,4 +1,5 @@
 import re
+import torch
 # keypoints (FloatTensor[N, K, 3]): the locations of the predicted keypoints, in [x, y, v] format.
 # During training, the model expects both the input tensors, as well as a targets (list of dictionary),
 #    containing:
@@ -21,11 +22,11 @@ for i, line in enumerate(file):
     if i == 0:
         for j, keypoint_label in enumerate(line.split(' ')):
             if j is not 0:
-                labels[j] = keypoint_label
+                labels[j] = torch.DoubleTensor(1)
     # Exracting keypoints, keypoints are stored after the first line of the file
     individual_cordinates = line.split(' ')[0]
     individual_cordinates = re.split(r'\t+', individual_cordinates)
-    print("Processing Metadata: " + str(int(i*100/34301)) + "%", end='\r')
+
     for j, cordinate in enumerate(individual_cordinates):
         count += 1        
         # Skip even iterations as including it duplicates (x,y) cordinates entry
@@ -33,21 +34,26 @@ for i, line in enumerate(file):
             continue
         # Skip the value in the first index of the list as this is the frame number    
         if j is not 0:
-            keypoints[int(count / 2)] = [i, [float(cordinate), float(individual_cordinates[j + 1]), 1], 3] 
+            keypoints[int(count / 2)] = [torch.as_tensor(i), torch.FloatTensor([float(cordinate), float(individual_cordinates[j + 1]), 1])]
             if j is 1:
                 # Obtain the boxes of each samples, theese are: xmin, xmax, ymin, ymax
                 xmin = float(cordinate)
                 xmax = float(cordinate)
                 ymin = float(individual_cordinates[j + 1])
                 ymax = float(individual_cordinates[j + 1])
-                boxes[i] = [i, [xmin, ymin, xmax, ymax]]
+                boxes[i] = torch.FloatTensor([float(xmin), float(ymin), float(xmax), float(ymax)])
             else:
                 xmin = xmin if (xmin < float(cordinate)) else float(cordinate)
                 xmax = xmax if (xmax > float(cordinate)) else float(cordinate)
                 ymin = ymin if (ymin < float(individual_cordinates[j + 1])) else float(individual_cordinates[j + 1])
                 ymax = ymax if (ymax > float(individual_cordinates[j + 1])) else float(individual_cordinates[j + 1])
-                boxes[i] = [i, [xmin, ymin, xmax, ymax]]
+                boxes[i] = torch.FloatTensor([float(xmin), float(ymin), float(xmax), float(ymax)])
 
 target.append(labels)
 target.append(keypoints)
 target.append(boxes)
+
+print(labels)
+
+targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
